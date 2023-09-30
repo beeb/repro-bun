a60c24a5726d4b1a9eca8a630e538b611bbc62b4
beeb <703631+beeb@users.noreply.github.com>
2023-09-30 09:30:37 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = { self, nixpkgs, utils, devenv, ... } @ inputs:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.outputs.legacyPackages.${system};
+      in
+      {
+        devShell = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            ({ pkgs, ... }: {
+              packages = with pkgs; [ bun ];
+              languages.javascript.enable = true;
+            })
+          ];
+        };
+      });
+}
